80f51ac7bec3945fea0c9739ba9d310f0eeab122
Joydip Das <joydipd381@gmail.com>
2024-06-07 10:36:44 +0600

    changes notice

--- TSC/views.py
+++ TSC/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,JsonResponse
 from django.contrib import messages
-# from TSC.models import*
+from TSC.models import*
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import user_passes_test,login_required
 from datetime import datetime
@@ -17,5 +17,18 @@ import re
 
 def Home(request):
     return render(request,"home.html")
-def Notice(request):
-    return render(request,"notice.html")
+def Notices(request):
+    notices = Notice.objects.all()
+    paginator=Paginator(notices,10)
+    page_no=request.GET.get('page')
+    try:
+        notices = paginator.page(page_no)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        notices = paginator.page('1')  
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        notices = paginator.page(paginator.num_pages)
+    return render(request, "notice.html", {'notices': notices,'page_name':"NOTICES"})
+
+
--- TSC_Website/urls.py
+++ TSC_Website/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TSC import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home,name='home'),
-    path('notice',views.Notice,name='notice'),
+    path('notice',views.Notices,name='notice'),
 
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
