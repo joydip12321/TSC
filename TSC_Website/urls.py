92a2c1fd594c1025958918ebebbf5e5b4bff093c
Joydip Das <joydipd381@gmail.com>
2024-10-30 15:12:54 +0600

    for deploy

--- TSC_Website/urls.py
+++ TSC_Website/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for TSC_Website project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from TSC import views
@@ -73,9 +58,6 @@ urlpatterns = [
     path('adminItem/',views.AdminItem,name="adminItem"),
     path('update_item/<int:item_id>',views.UpdateItem,name="update_item"),
     path('delete_item/<int:item_id>',views.DeleteItem,name="delete_item"),
-
-
-    
 
 
     path('Booking_list/',views.Booking_list,name="Booking_list"),
