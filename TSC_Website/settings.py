66e59f29d1737058373c2d77a88982a14bf025ef
Joydip Das <joydipd381@gmail.com>
2024-10-30 11:42:12 +0600

    done everything and ok

--- TSC_Website/settings.py
+++ TSC_Website/settings.py
@@ -126,6 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = 'C:/Users/CSE/Desktop/Joy/TSC/staticfiles'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
